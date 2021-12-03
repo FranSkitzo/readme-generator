47ee24480894a1cd58a80378c38f501089ca14fc
Rhogar <johnfmartinez597@gmail.com>
2021-12-03 15:15:17 -0500

    project setup

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello Node')
+// External packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+// Internal Modules
+const generateMarkdown = require('./utils/readme-template.js');
+const api = require('./utils/api.js');
